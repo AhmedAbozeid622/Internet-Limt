19a2ffeca1945d3bca1beab2f619a04a23104b1e
AhmedAbozeid622 <49274489+AhmedAbozeid622@users.noreply.github.com>
2022-03-06 23:50:21 +0200

    Update piy.py

--- piy.py
+++ piy.py
@@ -1,15 +1,14 @@
 import requests as r
 import sshtunnel
 
-sshtunnel.SSH_TIMEOUT = 0.1
 with sshtunnel.open_tunnel(
     ("ahmedmohsin622.duckdns.org", 2000),
     ssh_username="root",
-    ssh_pkey="private",
+    ssh_password="admin",
     remote_bind_address=("127.0.0.1", 80),
     local_bind_address=('0.0.0.0', 2000)
 ) as tunnel:
-    data = r.get(f"http://admin:@adminlocalhost:2000/Status_Wireless.live.asp").text
+    data = r.get(f"http://admin:admin@localhost:2000/Status_Wireless.live.asp").text
     print(data)
 
 print('FINISH!')
